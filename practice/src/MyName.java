b006069b7f2ca15288b5601a4bfc00363b9e1971
dusdn1702 <dusdn1702@naver.com>
2020-05-30 17:07:37 +0900

    정리

---
+++ practice/src/MyName.java
@@ -0,0 +1,7 @@
+public class MyName {
+    public static void main(String[] args) {
+        System.out.print("My name is ");
+        String name = "CHO YEONWOO";
+        System.out.print(name);
+    }
+}
