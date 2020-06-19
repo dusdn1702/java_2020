dfe2c908542f12fda677b6fc83e7cc56e38266fe
dusdn1702 <dusdn1702@naver.com>
2020-06-19 18:52:26 +0900

    자동차경주 추가

--- carculator/src/main/java/main.java
+++
@@ -1,9 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-        Carculator cal = new Carculator();
-        System.out.println(cal.add(3,4));
-        System.out.println(cal.subtract(5,4));
-        System.out.println(cal.multiply(2,6));
-        System.out.println(cal.divide(8,4));
-    }
-}
